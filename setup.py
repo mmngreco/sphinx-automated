97fed6bfb185f56531b741f446830d993f0f1c61
Maximiliano Greco <mgreco@ets.es>
2019-08-23 23:15:16 +0200

    BLD: Add dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="sphinx_automated",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "sphinx",
+        "sphinx_rtd_theme",
+        ],
 )
