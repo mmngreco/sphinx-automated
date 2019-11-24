4dcacdebc174e5e36d80d6ca5b6b8bb7392925f9
Maximiliano Greco <mgreco@ets.es>
2019-11-24 23:17:47 +0100

    ENH: Add config.ini process

--- sphinx_automated/run.py
+++ sphinx_automated/run.py
@@ -1,9 +1,42 @@
+import logging
+import configparser
+
 from pathlib import Path
+from . import (
+    customize_conf,
+    generate_html,
+    generate_rst,
+    make_doc_structure,
+    process_templates,
+    remove_doc_structure,
+    update_index,
+    utils,
+)
 
-from . import *
+logging.basicConfig(level=logging.INFO)
 
 
-def main(PRJ_DIR, SRC_NAME, PRJ_NAME, AUTHOR, clean):
+def read_config_file(path=None):
+    logging.info(f"{path}")
+    config = configparser.ConfigParser()
+
+    try:
+        config.read(Path(path).absolute().expanduser())
+    except:
+        PRJ_DIR = input("Enter project dir: ")
+        SRC_NAME = input("Enter a source folder name: ")
+        PRJ_NAME = input("Enter a project name [%s]: " % SRC_NAME)
+        AUTHOR = input("Enter author name: ")
+        config["project"] = dict(
+            PRJ_DIR=PRJ_DIR,
+            SRC_NAME=SRC_NAME,
+            PRJ_NAME=PRJ_NAME,
+            AUTHOR=AUTHOR,
+        )
+    return config
+
+
+def main(PRJ_DIR, SRC_NAME, PRJ_NAME, AUTHOR):
     PRJ_DIR = Path(PRJ_DIR)
     SRC_DIR = Path(PRJ_DIR) / SRC_NAME
 
@@ -25,19 +58,29 @@ def main(PRJ_DIR, SRC_NAME, PRJ_NAME, AUTHOR, clean):
     update_index.main(PRJ_DIR)
     generate_html.main(PRJ_DIR, SRC_NAME)
 
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-c", "--config-file", help="Path to config.ini file.", default="~/.sphinx_automated.conf")
+    parser.add_argument(
+        "-c",
+        "--config-file",
+        help="Path to config.ini file.",
+        default="~/.sphinx_automated.conf",
+    )
     parser.add_argument("--prj-dir")
     parser.add_argument("--src-name")
     parser.add_argument("--prj-name")
     parser.add_argument("--author")
-    parser.add_argument("-f","--create-config-file", help="Create an example config file in the path passed.")
-    parser.add_argument("--clean", help="Remove existing content and create it again from scratch.",
-            default=False)
+    parser.add_argument(
+        "--create-config-file",
+        help="Create an example config file in the path passed.",
+    )
+    parser.add_argument(
+        "--clean", help="Overwrites existing doc content.", default=False
+    )
 
     args = parser.parse_args()
 
@@ -55,5 +98,6 @@ if __name__ == "__main__":
             SRC_NAME = args.src_name
             PRJ_NAME = args.prj_name
             AUTHOR = args.author
+
         clean = args.clean
         main(PRJ_DIR, SRC_NAME, PRJ_NAME, AUTHOR, clean)
