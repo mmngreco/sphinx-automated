f310c79457a894c2575a9a1c593d3bbfbd1d2281
Maximiliano Greco <mgreco@ets.es>
2020-03-09 09:24:33 +0100

    BUG: fix config creation

--- sphinx_automated/run.py
+++ sphinx_automated/run.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("--src-name")
     parser.add_argument("--prj-name")
     parser.add_argument("--author")
-    parser.add_argument("--create-config-file", help="Create an example config file in the path passed.")
+    parser.add_argument("-f","--create-config-file", help="Create an example config file in the path passed.")
     parser.add_argument("--clean", help="Remove existing content and create it again from scratch.",
             default=False)
 
@@ -43,17 +43,17 @@ if __name__ == "__main__":
 
     if args.create_config_file is not None:
         utils.write_config(args.create_config_file)
-
-    try:
-        config = utils.read_config(args.config_file)
-        PRJ_DIR = config["DEFAULT"]["prj_dir"]
-        SRC_NAME = config["DEFAULT"]["src_dir"]
-        PRJ_NAME = config["DEFAULT"]["prj_name"]
-        AUTHOR = config["DEFAULT"]["author"]
-    except KeyError:
-        PRJ_DIR = args.prj_dir
-        SRC_NAME = args.src_name
-        PRJ_NAME = args.prj_name
-        AUTHOR = args.author
-    clean = args.clean
-    main(PRJ_DIR, SRC_NAME, PRJ_NAME, AUTHOR, clean)
+    else:
+        try:
+            config = utils.read_config(args.config_file)
+            PRJ_DIR = config["DEFAULT"]["prj_dir"]
+            SRC_NAME = config["DEFAULT"]["src_dir"]
+            PRJ_NAME = config["DEFAULT"]["prj_name"]
+            AUTHOR = config["DEFAULT"]["author"]
+        except KeyError:
+            PRJ_DIR = args.prj_dir
+            SRC_NAME = args.src_name
+            PRJ_NAME = args.prj_name
+            AUTHOR = args.author
+        clean = args.clean
+        main(PRJ_DIR, SRC_NAME, PRJ_NAME, AUTHOR, clean)
